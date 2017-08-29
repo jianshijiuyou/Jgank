c85848602c52bc93b7f8ca1127916c19f56a61fd
jianshijiuyou <jianshijiuyou@gmail.com>
2017-08-29 17:33:55 +0800

    update

---
+++ pages/index/index.js
@@ -0,0 +1,35 @@
+Page({
+  data:{
+    list:[],
+    classify: [{ id: 0, name: "推荐" }, 
+    { id: 1, name: "福利" }, 
+    { id: 2, name: "Android" }, 
+    { id: 3, name: "ios" },
+    { id: 4, name: "休息视频" }, 
+    { id: 5, name: "拓展资源" }, 
+    { id: 6, name: "前端" }],
+    curTab: 0
+  },
+  onReady:function(){
+    // var page = this
+    // wx.request({
+    //   url: 'https://gank.io/api/data/%E7%A6%8F%E5%88%A9/10/1', 
+    //   method:"GET",
+    //   success: function (res) {
+    //     console.log("=========success========")
+    //     console.log(res.data)
+    //     page.setData({
+    //       list: res.data.results
+    //     })
+    //   }
+    // })
+  },
+  classifyClick:function(e){
+    if (this.data.curTab == e.currentTarget.dataset.type){
+      return
+    }
+    this.setData({
+      curTab: e.currentTarget.dataset.type
+    })
+  }
+})
