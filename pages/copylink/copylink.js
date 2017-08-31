33b67c30bcc404dce06147cb4551239383a5d23f
jianshijiuyou <jianshijiuyou@gmail.com>
2017-08-31 14:59:03 +0800

    推荐列表完成

---
+++ pages/copylink/copylink.js
@@ -0,0 +1,30 @@
+// index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+    this.setData({
+      url: options.url
+    })
+  },
+  copyBtnClick:function(e){
+    var url = e.currentTarget.dataset.url
+    wx.setClipboardData({
+      data:url,
+      success: function (res) {
+        wx.showToast({
+          title: '已复制'
+        })
+      }
+    })
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,6 @@
 Page({
   data:{
-    list:[],
+    recommend: null,
     classify: [{ id: 0, name: "推荐" }, 
     { id: 1, name: "福利" }, 
     { id: 2, name: "Android" }, 
@@ -11,26 +11,58 @@ Page({
     curTab: 0
   },
   onReady:function(){
-    // var page = this
-    // wx.request({
-    //   url: 'https://gank.io/api/data/%E7%A6%8F%E5%88%A9/10/1', 
-    //   method:"GET",
-    //   success: function (res) {
-    //     console.log("=========success========")
-    //     console.log(res.data)
-    //     page.setData({
-    //       list: res.data.results
-    //     })
-    //   }
-    // })
+    var page = this
+    wx.showLoading({
+      title: "加载中",
+      mask: true
+    })
+    wx.request({
+      url: encodeURI('https://gank.io/api/day/history'), 
+      method:"GET",
+      success: function (res) {
+        
+        var url = "https://gank.io/api/day/"+res.data.results[0].replace(/-/g, "/")
+        page.requestRecommend(encodeURI(url))
+      },
+      fail:function(){
+        wx.hideLoading()
+      }
+    })
   },
   classifyClick:function(e){
     if (this.data.curTab == e.currentTarget.dataset.type){
       return
     }
-    console.log("切换")
     this.setData({
       curTab: e.currentTarget.dataset.type
+    })
+  },
+  requestRecommend:function(url){
+    var page = this
+    wx.request({
+      url: url,
+      method: "GET",
+      success: function (res) {
+        wx.hideLoading()
+        page.setData({
+          recommend: res.data.results
+        })
+      },
+      fail: function () {
+        wx.hideLoading()
+      }
+    })
+  },
+  imgClick:function(e){
+    var url = e.currentTarget.dataset.imgUrl
+    wx.previewImage({
+      urls: [url] 
+    })
+  },
+  itemClick:function(e){
+    var url=e.currentTarget.dataset.url
+    wx.navigateTo({
+      url: "../copylink/copylink?url=" + url
     })
   }
 })
